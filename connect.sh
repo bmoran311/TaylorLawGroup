d873ea5b51596d3ae58f300016914e938b4f0e1f
bmoran311 <youremail@example.com>
2024-12-20 13:13:42 -0500

    bmoran.pub

--- connect.sh
+++ connect.sh
@@ -1,2 +1,6 @@
 echo 'Connecting to TaylorLaw Staging Database Server...'
-ssh -i id_rsa.pub -L 5555:10.116.0.2:3306 forge@165.227.195.171
+ssh -i bmoran.pub -L 5555:10.116.0.2:3306 forge@165.227.195.171
+
+echo "Starting sleep..."
+sleep 10
+echo "10 seconds later..."
