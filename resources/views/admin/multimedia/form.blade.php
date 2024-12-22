3bf6e5df575e84686198cee122bac8e9ecd44cbb
bmoran311 <youremail@example.com>
2024-12-22 11:26:27 -0500

    Organized multimedia better.

--- resources/views/admin/multimedia/form.blade.php
+++ resources/views/admin/multimedia/form.blade.php
@@ -69,7 +69,7 @@
                             <textarea
                                 name="summary"
                                 rows="6"
-                                placeholder="Active textarea"
+                                placeholder="Summary..."
                                 class="w-full rounded-lg border-[1.5px] border-primary bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
                             >{{ old('summary', $multimedia->summary ?? '') }}</textarea>                            
                         </div>
