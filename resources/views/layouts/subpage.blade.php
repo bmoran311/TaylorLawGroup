5bcf36a0ec477cb6945e07c7c9fa6e42c84b8e22
= <=>
2025-03-13 06:12:40 -0400

    updates to layout

--- resources/views/layouts/subpage.blade.php
+++ resources/views/layouts/subpage.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 @include('partials.head')
+
 <body>
-	<header>	
+    <header style="margin-top: 50px;">
         <x-header.header />
         <x-main.banner :h1Text="$headerInfo['h1Text']" :h4Text="$headerInfo['h4Text']" :bannerText="$headerInfo['bannerText']" />
     </header>
 
-    @yield('content') 
+    @yield('content')
 
-	<x-home.testimonials />   
-    <x-footer.contact />      
+    <x-home.testimonials />
+    <x-footer.contact />
 
     <footer>
-		<x-footer.footer />        	
-	</footer>
+        <x-footer.footer />
+    </footer>
 </body>
+
 </html>
