b31d7bc48ab87750bf97ae06e0d5f4dee9304683
= <=>
2025-03-17 07:13:13 -0400

    link change and quote change

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,16 +1,10 @@
 <?php
-    use Illuminate\Support\Facades\DB;
-    $bios = DB::table('bio')
-        ->where('type', 'LIKE', '%Attorney%')
-        ->orderBy('sort_order', 'asc')
-        ->get();
+use Illuminate\Support\Facades\DB;
+$bios = DB::table('bio')->where('type', 'LIKE', '%Attorney%')->orderBy('sort_order', 'asc')->get();
 
-    $leaders = DB::table('bio')
-        ->where('type', 'LIKE', '%Leadership%')
-        ->orderBy('sort_order', 'asc')
-        ->get();
+$leaders = DB::table('bio')->where('type', 'LIKE', '%Leadership%')->orderBy('sort_order', 'asc')->get();
 
-    $practice_areas = DB::table('practice_area')->latest()->get();
+$practice_areas = DB::table('practice_area')->latest()->get();
 ?>
 
 <div class="header-top">
@@ -21,25 +15,28 @@
                     <div class="header-clock-cont">
                         <ul class="list-unstyled d-inline-flex d-flex">
                             <li><a href="#"></a><br></li>
-                            <li><h4>&nbsp;</h4><br>
-                            <p><br></p>
+                            <li>
+                                <h4>&nbsp;</h4><br>
+                                <p><br></p>
                             </li>
                         </ul>
-						<!---
-						<ul class="list-unstyled d-inline-flex d-flex">
+                        <!---
+      <ul class="list-unstyled d-inline-flex d-flex">
                             <li><a href="#"><i class="fa-solid fa-clock"></i></a></li>
                             <li><h4>Mon - Fri: <span>8:00am  - 5:00 pm</span></h4>
                             <p>Answering services avaliable on weekends and offer hours</p>
                             </li>
                         </ul>
-						--->
+      --->
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="header-clock-cont text-end">
                         <ul class="list-unstyled d-inline-flex d-flex">
-							<li><a href="tel: 843.723.2000"><i class="fa-solid fa-phone"></i></a></li>
-                            <li><h4>Call Us <span> 843.723.2000</span></h4></li>
+                            <li><a href="tel: 843.723.2000"><i class="fa-solid fa-phone"></i></a></li>
+                            <li>
+                                <h4>Call Us <span> 843.723.2000</span></h4>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -52,11 +49,14 @@
             <div class="mainmenu">
                 <nav class="navbar navbar-expand-md">
                     <div class="container">
-                        <a class="navbar-brand d-block d-md-none d-lg-none d-sm-block" href="index.html"><img src="/site/img/logo.png" class="img-fluid" alt="logo" /></a>
+                        <a class="navbar-brand d-block d-md-none d-lg-none d-sm-block" href="index.html"><img
+                                src="/site/img/logo.png" class="img-fluid" alt="logo" /></a>
                         <div class="get-start text-end d-block d-md-none d-lg-none d-sm-block">
                             <a href="login.html" class="btn">Get Started</a>
                         </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -68,50 +68,64 @@
                                     <a class="nav-link active" aria-current="page" href="/our-firm">Our Firm </a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="/attorneys?type=Attorney" id="navbarDropdown" role="button" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="/attorneys?type=Attorney"
+                                        id="navbarDropdown" role="button" aria-expanded="false">
                                         Attorneys
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach ($bios as $bio)
-                                            <li><a class="dropdown-item" href="/attorney-detail/{{ $bio->id }}">{{ $bio->first_name }} {{ $bio->last_name }}</a></li>
+                                            <li><a class="dropdown-item"
+                                                    href="/attorney-detail/{{ $bio->id }}">{{ $bio->first_name }}
+                                                    {{ $bio->last_name }}</a></li>
                                         @endforeach
                                     </ul>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="/practice-areas" id="navbarDropdown" role="button" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="/practice-areas" id="navbarDropdown"
+                                        role="button" aria-expanded="false">
                                         Firm Expertise
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach ($practice_areas as $practice_area)
-                                            <li><a class="dropdown-item" href="/practice-area/{{ $practice_area->id }}">{{ $practice_area->name }}</a></li>
+                                            <li><a class="dropdown-item"
+                                                    href="/practice-area/{{ $practice_area->id }}">{{ $practice_area->name }}</a>
+                                            </li>
                                         @endforeach
                                     </ul>
                                 </li>
                                 <li class="nav-item logo-t">
-                                    <a class="nav-link" href="/"><img src="/site/img/logo1.png" class="img-fluid" alt="logo" /></a>
+                                    <a class="nav-link" href="/"><img src="/site/img/logo1.png" class="img-fluid"
+                                            alt="logo" /></a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="/attorneys?type=Leadership" id="navbarDropdown" role="button" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="/attorneys?type=Leadership"
+                                        id="navbarDropdown" role="button" aria-expanded="false">
                                         Leadership
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         @foreach ($leaders as $leader)
-                                            <li><a class="dropdown-item" href="/attorney-detail/{{ $leader->id }}">{{ $leader->first_name }} {{ $leader->last_name }}</a></li>
+                                            <li><a class="dropdown-item"
+                                                    href="/attorney-detail/{{ $leader->id }}">{{ $leader->first_name }}
+                                                    {{ $leader->last_name }}</a></li>
                                         @endforeach
                                     </ul>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="/our-firm" id="navbarDropdown" role="button" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="/our-firm" id="navbarDropdown"
+                                        role="button" aria-expanded="false">
                                         Culture
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a class="dropdown-item" href="/testimonials?type=Customer Service">Customer Service</a></li>
-                                        <li><a class="dropdown-item" href="/testimonials?type=Experience">Experience</a></li>
-                                        <li><a class="dropdown-item" href="/testimonials?type=Commitment">Commitment</a></li>
+                                        <li><a class="dropdown-item" href="/testimonials?type=Customer Service">Customer
+                                                Service</a></li>
+                                        <li><a class="dropdown-item" href="/testimonials?type=Experience">Experience</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="/testimonials?type=Commitment">Commitment</a>
+                                        </li>
                                     </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="/news">Latest </a>
+                                    <a class="nav-link" aria-current="page" href="/news">Client Portal </a>
                                 </li>
                                 <!---
                                 <li class="nav-item">
--- resources/views/site/testimonials.blade.php
+++ resources/views/site/testimonials.blade.php
@@ -1,36 +1,41 @@
-
 @extends('layouts.subpage')
 
 @section('content')
-<div class="industry-section">
-    <div class="taylorline-about-left">
-        <img src="/site/img/others/taylor-line-left.png" class="img-fluid" alt="taylorline" />
-    </div>
-    <div class="container">
-        <div class="about-wrap heading">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-                    <div class="tab-content" id="v-pills-tabContent">
-                        <div class="tab-right-content heading">
-                            <h3>{{ $type }}</h3>
-                            <hr class="border-bot">
-                            <div class="row">
-                                @foreach ($testimonials as $testimonial)
-                                    <div class="col-md-6 col-sm-12 mb-4">
-                                        <a href="/testimonial-detail/{{ $testimonial->id }}" style="text-decoration: none;">
-                                            <div class="testimonial-content p-4 position-relative">
-                                                <div class="quote-icon position-absolute top-2 start-0 translate-middle">
-                                                    <i class="fas fa-quote-left" style="color: #c5a183; font-size: 1.5rem;"></i>
-                                                </div>                                                                                                
-                                                <p class="fst-italic text-secondary m-0" style="text-transform: none; font-size: 0.95rem;">{{ $testimonial->summary }}</p>
-                                                <div class="quote-icon position-absolute bottom-0 end-0 translate-middle">
-                                                    <i class="fas fa-quote-right" style="color: #c5a183; font-size: 1.5rem;"></i>
+    <div class="industry-section">
+        <div class="taylorline-about-left">
+            <img src="/site/img/others/taylor-line-left.png" class="img-fluid" alt="taylorline" />
+        </div>
+        <div class="container">
+            <div class="about-wrap heading">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+                        <div class="tab-content" id="v-pills-tabContent">
+                            <div class="tab-right-content heading">
+                                <h3>{{ $type }}</h3>
+                                <hr class="border-bot">
+                                <div class="row">
+                                    @foreach ($testimonials as $testimonial)
+                                        <div class="col-md-6 col-sm-12 mb-4">
+                                            <a href="/testimonial-detail/{{ $testimonial->id }}"
+                                                style="text-decoration: none;">
+                                                <div class="testimonial-content p-4 position-relative">
+                                                    <p class="fst-italic text-secondary m-0"
+                                                        style="text-transform: none; font-size: 0.95rem;">
+                                                        <span class="quote-icon" style="color: #c5a183; font-size: 1.5rem;">
+                                                            <i class="fas fa-quote-left"></i>
+                                                        </span>
+                                                        {{ $testimonial->summary }}
+                                                        <span class="quote-icon" style="color: #c5a183; font-size: 1.5rem;">
+                                                            <i class="fas fa-quote-right"></i>
+                                                        </span>
+                                                    </p>
+                                                    <h4 class="text-dark fw-semibold fs-6 mb-1">
+                                                        {{ $testimonial->client_name }} - {{ $testimonial->title }}</h4>
                                                 </div>
-                                                <h4 class="text-dark fw-semibold fs-6 mb-1">{{ $testimonial->client_name }} - {{ $testimonial->title }}</h4>
-                                            </div>
-                                        </a>
-                                    </div>
-                                @endforeach
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -38,5 +43,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
